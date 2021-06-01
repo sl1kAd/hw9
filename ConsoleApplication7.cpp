2dffc8f73bf4c27154d0b99ccf0b4a20728b5eb7
sl1kAd <80388890+sl1kAd@users.noreply.github.com>
2021-06-01 14:13:03 +0300

    Add files via upload

---
+++ ConsoleApplication7.cpp
@@ -0,0 +1,72 @@
+﻿
+#include <stdio.h>
+#include "QueuePriority.h"
+
+int errorQueuePriority;
+
+void initQueuePriority(QueuePriority* F) 
+{
+	F->uk = 0;
+	errorQueuePriority = okQueuePriority;
+}
+
+void putQueuePriority(QueuePriority* F, queuePriorityBaseType E) 
+{
+	
+	if (isFullQueuePriority(F)) 
+	{
+		return;
+	}
+	
+	F->buf[F->uk] = E;
+	F->uk++;
+}
+
+void getQueuePriority(QueuePriority* F, queuePriorityBaseType* E) 
+{
+	
+	if (isEmptyQueuePriority(F)) 
+	{
+		return;
+	}
+	
+	queuePriorityBaseType max = F->buf[0];		
+
+	int maxPos = 0;	
+
+	for (unsigned i = 1; i < F->uk; i++) 
+	{			
+		if (F->buf[i].priority > max.priority) 
+		{		
+			max = F->buf[i];
+			maxPos = i;
+		}
+	}
+	
+	*E = max;											
+	F->buf[maxPos] = F->buf[--F->uk];					
+	
+}
+
+int isFullQueuePriority(QueuePriority* F) 
+{
+	if (F->uk == SIZE_QUEUE_PRIORITY) 
+	{
+		errorQueuePriority = fullQueuePriority;
+
+		return 1;
+	}
+
+	return 0;
+}
+
+int isEmptyQueuePriority(QueuePriority* F) 
+{
+	if (F->uk == 0) 
+	{
+		errorQueuePriority = emptyQueuePriority;
+
+		return 1;
+	}
+	return 0;
+}
